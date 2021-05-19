0783663b179e5b7a295b10f2105cfa3054cb5f67
dskkato <dkato@topcon.com>
2021-05-20 08:30:49 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Draw sample'),
+          title: Text('Draw sample2'),
         ),
         body: DrawingArea());
   }
@@ -38,17 +38,12 @@ Future<ui.FrameInfo> _loadImage(AssetBundle bundle, String path) async {
   final data = await bundle.load(path);
   final bytes = data.buffer.asUint8List();
   final codec = await ui.instantiateImageCodec(bytes);
-  final frameInfo = await codec.getNextFrame();
-  final buf = await frameInfo.image.toByteData();
-  final buffer = buf!.buffer.asUint8List();
-  final immutableBuffer = await ui.ImmutableBuffer.fromUint8List(buffer);
-  final imageDescriptor = ui.ImageDescriptor.raw(immutableBuffer,
-      width: 400, height: 600, pixelFormat: ui.PixelFormat.rgba8888);
-  final codec2 = await imageDescriptor.instantiateCodec();
-  return codec2.getNextFrame();
+  return codec.getNextFrame();
 }
 
 class _DrawingAreaState extends State<DrawingArea> {
+  final TransformationController _transformationController =
+      TransformationController();
   @override
   void initState() {
     super.initState();
@@ -62,14 +57,36 @@ class _DrawingAreaState extends State<DrawingArea> {
       builder: (context, snapshot) {
         Widget child;
         if (snapshot.hasData) {
-          child = CustomPaint(
-            painter: DrawingPainter(snapshot.data!.image),
+          var image = snapshot.data!.image;
+          child = InteractiveViewer(
+            //boundaryMargin: const EdgeInsets.all(20.0),
+            minScale: 0.1,
+            maxScale: 5.0,
+            //clipBehavior: Clip.none,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: <Color>[Colors.orange, Colors.red],
+                      stops: <double>[0.0, 1.0],
+                    ),
+                  ),
+                ),
+                CustomPaint(
+                  painter: DrawingPainter(image),
+                ),
+              ],
+            ),
           );
         } else if (snapshot.hasError) {
           child = Text('error occurred');
         } else {
           child = Text('waiting...');
         }
+        print('$child');
         return child;
       },
     );
